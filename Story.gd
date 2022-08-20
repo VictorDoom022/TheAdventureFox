831a94ea3bb20aa1a55250302f9b2d278eac4118
unknown <victordoom022@gmail.com>
2022-08-21 00:57:38 +0800

    Story line changes

--- Story.gd
+++ Story.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 var count = 0
 var story = [
-	"Adventure Fox is just an ordinary little fox, It is the eldest brother, two sisters and parents in the family, and the family lives happily.the parents and the younger sisters not being able to hunt, the little fox must take on the heavy responsibility of foraging and find food for the family.",
-	"One day, when the fox came back from hunting as usual, he found that his home had been destroyed, his parents and sister were dead. The little fox is very sad.",
-	"The little fox ran to ask the 'sensible dog' outside the green forest for details. Only then did the little fox know that a group of mutated bats had come to this green forest, and many animals had already suffered.",
-	"It wanted to avenge the lost family, so it went to the village where humans lived and wanted to learn from humans how to defeat bats. The little fox was very lucky, it met a family and adopted him. ",
-	"A year later, the little fox has learned a lot from humans and said goodbye to the humans who adopted it. The little fox is grateful for them, but it has to avenge the family. So the little fox returned to the green forest and started his own adventure.",
+	"The Adventure Fox is just an ordinary little fox. He is the eldest among the siblings. He lives happily with his family. But...due to his parent's old age, he has to take on the heavy responsibility of foraging and finding food for the family",
+	"One day, when the fox came back from hunting, as usual. He found that his home had been destroyed, and his family members were all dead. The little fox is very sad and furious.",
+	"The little fox ran to ask the 'sensible dog' outside the green forest for details. Only then did the little fox know that a group of mutated bats had come to this green forest, and many animals have died.",
+	"He wanted to avenge his lost family, so he went to the village where humans lived and learned how to defeat the bats from them. The little fox was very lucky, he met a family and adopted him. ",
+	"A year later, the little fox has learned a lot from humans and said goodbye to the humans who adopted him. The little fox is grateful for them, but he has to avenge his family. So the little fox returned to the green forest and started his own adventure. Here starts the journey of his revenge..." 
 ]
 
 
